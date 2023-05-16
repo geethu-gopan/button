9d9d9e13ffb78b4cc380bc3fc9e5cbd7c5c685cc
geethu-gopan <geethuavanthika2432@gmail.com>
2023-05-16 14:46:02 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:button/crud.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MaterialApp(debugShowCheckedModeBanner: false,home:crud()));
+}
